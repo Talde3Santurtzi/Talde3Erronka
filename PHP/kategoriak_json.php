b34f775b01155d0834f993c7fc06e67cb5bb989a
Ander <115074583+EeeeSIGracias@users.noreply.github.com>
2023-11-20 12:36:26 +0100

    asd

---
+++ PHP/kategoriak_json.php
@@ -0,0 +1,178 @@
+<?php
+    //si no se pone esto, no va a funcionar en el server
+    header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");    
+    include "db_konexioa.php";
+
+    $db = new Datubasea();
+    /**
+    * Kategoria klasea.
+    *
+    * @class Kategoria
+    */
+    class Kategoria
+    {
+        /**
+        * @var int $id Kategoriaren id-a.
+        */
+        public $id;
+        /**
+        * @var string $izena Kategoriaren izena.
+        */
+        public $izena;
+        /**
+        * Kategoria klasearen eraikitzailea.
+        *
+        * @method __construct
+        * @param int $id Kategoriaren id-a.
+        * @param string $izena Kategoriaren izena.
+        */
+        public function __construct($id, $izena)
+        {
+            $this->id = $id;
+            $this->izena = $izena;
+        }
+        /**
+         * Kategoriaren id-a lortzeko.
+         *
+         * @return int Kategoriaren id-a.
+         */
+        public function getId()
+        {
+            return $this->id;
+        }
+        /**
+         * Kategoriaren izena lortzeko.
+         *
+         * @return string Kategoriaren izena
+         */
+        public function getIzena()
+        {
+            return $this->izena;
+        }
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $json_data = json_decode(file_get_contents("php://input"), true);
+        if (isset($_GET["num"])) {
+            $emaitzak = lortuKategoriaById($_GET["num"]);
+            echo json_encode($emaitzak);
+        } else {
+            $emaitzak = lortuKategoriak();
+            echo json_encode($emaitzak);
+        }
+        exit;
+    } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $json_data = json_decode(file_get_contents("php://input"), true);
+        $emaitzak = txertatuKategoria($json_data["izena"]);
+        echo json_encode($emaitzak);
+    } elseif ($_SERVER["REQUEST_METHOD"] == "PUT") {
+        $json_data = json_decode(file_get_contents("php://input"), true);
+        if (isset($json_data["id"], $json_data["izena"])) {
+            $id = $json_data["id"];
+            $izena = $json_data["izena"];
+            eguneratuKategoria($id, $izena);
+        }
+    } elseif ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+        $json_data = json_decode(file_get_contents("php://input"), true);
+        if (isset($json_data)) {
+            $id = $json_data;
+            foreach ($id as $value) {
+                ezabatuKategoria($value);
+            }
+            echo "OK";
+        }
+    }
+    /**
+     * Kategoria bat ezabatzeko datu basetik eta honen ekipoak.
+     *
+     * @param int $id Kategoriaren id-a ezabatzeko.
+     * @return void
+     */
+    function ezabatuKategoria($id)
+    {
+        global $db;
+        $sql = "DELETE FROM ekipamendua WHERE idKategoria = '$id'";
+        $db->ezabatu($sql);
+        $sql = "DELETE FROM kategoria WHERE id = '$id'";
+        $db->ezabatu($sql);
+    }
+    /**
+     * Kategoria baten izena eguneratzeko.
+     *
+     * @param int $id ID de la categoría a actualizar.
+     * @param string $izena Nuevo nombre de la categoría.
+     * @return void
+     */
+    function eguneratuKategoria($id, $izena)
+    {
+        global $db;
+        $sql = "UPDATE kategoria SET izena = '$izena' WHERE id = '$id'";
+        $db->eguneratu($sql);
+    }
+    /**
+     * Kategoria berria sartzeko dat basean.
+     *
+     * @param string $izena Kategoria berriaren izena.
+     * @return void
+     */
+    function txertatuKategoria($izena)
+    {
+        global $db;
+        $emaitzak = $db->datuakLortu("SELECT izena from kategoria");
+        $bul = true;
+        $kategoriak = array();
+        if (is_object($emaitzak)) {
+            while ($row = $emaitzak->fetch_assoc()) {
+                $kategoriak = $row["izena"];
+                if($row["izena"] == $izena){
+                    $bul = false;
+                }
+            }
+            if ($bul){
+                $sql = "INSERT INTO kategoria (izena) VALUES ('$izena')";
+                $db->txertatu($sql);
+            }else{
+                return "Kategoria hori jada existitzen da. Probatu beste izenarekin.";
+            }
+        }else{
+            return "Errorea txertatzean";
+        }
+    }
+    /**
+     * Kategoria array bat lortzen du.
+     *
+     * @return array Kategoriak gordetzeko array-a.
+     */
+    function lortuKategoriak()
+    {
+        global $db;
+        $emaitzak = $db->datuakLortu("SELECT * FROM kategoria");
+        $kategoriak = array();
+        if (is_object($emaitzak)) {
+            while ($row = $emaitzak->fetch_assoc()) {
+                $kategoriak[] = new Kategoria($row["id"], $row["izena"]);
+            }
+            return $kategoriak;
+        }
+    }
+    /**
+     * Kategoria bat lortzen du id-ekin.
+     *
+     * @param int $id Kategoriaren id-a.
+     * @return array Kategoriaren array-a.
+     */
+    function lortuKategoriaById($id)
+    {
+        global $db;
+        $emaitzak = $db->datuakLortu("SELECT * FROM kategoria WHERE id = '$id'");
+        $kategoriak = array();
+        if (is_object($emaitzak)) {
+            while ($row = $emaitzak->fetch_assoc()) {
+                $kategoriak[] = new Kategoria($row["id"], $row["izena"]);
+            }
+            return $kategoriak;
+        }
+    }
+?>
---
+++ PHP/logina.php
@@ -0,0 +1,163 @@
+<?php
+    //si no se pone esto, no va a funcionar en el server
+    header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");    
+    include "db_konexioa.php";
+    $db = new Datubasea();
+    /**
+    * Erabiltzaile klasea.
+    *
+    * @class Erabiltzaile
+    */
+    class Erabiltzaile{
+        /**
+        * @var int $nan Erabiltzailearen nan-a.
+        */
+        private $nan;
+        /**
+        * @var string $izena Erabiltzailearen izena.
+        */
+        private $izena;
+        /**
+        * @var string $abizena Erabiltzailearen abizena.
+        */
+        private $abizena;
+        /**
+        * @var string $erabiltzailea Erabiltzailea.
+        */
+        private $erabiltzailea;
+        /**
+        * @var string $pasahitza Erabiltzailearen pasahitza.
+        */
+        private $pasahitza;
+        /**
+        * @var string $rola Erabiltzailearen rola.
+        */
+        private $rola;
+        /**
+        * Erabiltzaile klasearen eraikitzailea.
+        *
+        * @method __construct
+        * @param int $nan Erabiltzailearen nan-a.
+        * @param string $izena Erabiltzailearen izena.
+        * @param string $abizena Erabiltzailearen abizena.
+        * @param string $erabiltzailea Erabiltzailea.
+        * @param string $pasahitza Erabiltzailearen pasahitza.
+        * @param string $rola Erabiltzailearen rola.
+        */
+        public function __construct($nan, $izena, $abizena, $erabiltzailea, $pasahitza, $rola) {
+            $this->nan = $nan;
+            $this->izena = $izena;
+            $this->abizena = $abizena;
+            $this->erabiltzailea = $erabiltzailea;
+            $this->pasahitza = $pasahitza;
+            $this->rola = $rola;
+        }
+
+        // public function getNan(){
+        //     return $this->nan;
+        // }
+
+        // public function getIzena(){
+        //     return $this->izena;
+        // }
+
+        // public function getAbizena(){
+        //     return $this->abizena;
+        // }
+        /**
+         * Erabiltzailea lortzeko.
+         *
+         * @return string Erabiltzailea.
+         */
+        public function getErabiltzailea(){
+            return $this->erabiltzailea;
+        }
+        /**
+         * Erabiltzailearen pasahitza lortzen du.
+         *
+         * @return string Pasahitza.
+         */
+        public function getPasahitza(){
+            return $this->pasahitza;
+        }
+        /**
+         * Erabiltzailearen rola lortzeko.
+         *
+         * @return string Erabiltzailearen rola.
+         */
+        public function getRola(){
+            return $this->rola;
+        }
+
+
+        // public function getRola(){
+        //     return $this->rola;
+        // }
+    }
+
+    if ($_SERVER["REQUEST_METHOD"]=="POST"){
+        $json_data = json_decode(file_get_contents("php://input"), true);
+        if (isset($json_data)){
+            erabiltzaileExists($json_data["erabiltzailea"],$json_data["pasahitza"]);
+
+        }
+    }
+    /**
+     * Erabiltzailearen datuak lortzen ditu.
+     *
+     * @return array Datuak gordetzeko array-a.
+     */
+    function lortuDatuak()
+    {
+        global $db;
+        $emaitzak = $db->datuakLortu("SELECT * FROM erabiltzailea");
+        $erabiltzaileak = array();
+
+        if ($emaitzak !== null) {
+            while ($row = $emaitzak->fetch_assoc()) {
+                $erabiltzaileak[] = new Erabiltzaile($row["nan"], $row["izena"], $row["abizena"], $row["erabiltzailea"], $row["pasahitza"], $row["rola"]);
+            }
+        }
+        return $erabiltzaileak;
+    }
+    /**
+     * Jakitzeko erabiltzailea eta pasahitza ondo dauden.
+     *
+     * @param string $erabiltzailea Erabiltzailea.
+     * @param string $pasahitza Erabiltzailearen pasahitza.
+     * @return void
+     */
+    function erabiltzaileExists($erabiltzailea, $pasahitza)
+    {
+        $erabiltzaileak = lortuDatuak();
+        $bool = false;
+        foreach ($erabiltzaileak as $erabiltzaile) {
+            if ($erabiltzaile->getErabiltzailea() == $erabiltzailea) {
+                if ($pasahitza == $erabiltzaile->getPasahitza()) {
+                    // Erabiltzailea eta pasahitza zuzenak dira
+                    // echo "Sesioan sartu zara.";
+                    // return true;
+                    $rol=$erabiltzaile->getRola();
+                    echo json_encode("Erabiltzailea_eta_pasahitza_ondo_sartuta_daude,".$rol.",");
+                } else {
+                    // Pasahitza okerra
+                    // echo "Pasahitza okerra.";
+                    // return false;
+                    echo json_encode("Pasahitza_txarto_sartuta_dago");
+                }
+                $bool = true;
+            }
+            //else{
+                // Erabiltzailea ez da aurkitzen
+                // echo "Erabiltzaile hori ez da existitzen.";
+                // return false;
+            //}
+        }
+        if (!$bool) {
+            echo json_encode("Erabiltzailea_ez_da_existitzen");
+        }
+        
+    }
+?>
